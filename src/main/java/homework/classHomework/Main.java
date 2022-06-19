a0c4da27a1f389b9e195dc883bb4511ddc4d7773
LeldeZake-Kazaka <leldezakekazaka@gmail.com>
2022-06-19 18:52:21 +0300

    [feature/java]: Add HomeWork 7

---
+++ src/main/java/homework/classHomework/Main.java
@@ -0,0 +1,11 @@
+package homework.classHomework;
+
+public class Main {
+    public static void main(String[] args) {
+        trīsstūris myTreangle = new trīsstūris();
+        trīsstūris myTreangleArea = new trīsstūris(5,5,5);
+        trīsstūris myTreangleTwo = new trīsstūris(5,5,5,5);
+        System.out.println(myTreangleTwo);
+    }
+
+}
